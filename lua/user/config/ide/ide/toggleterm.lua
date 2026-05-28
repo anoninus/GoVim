5ee7889cea66b12eac8e77e9d48b9e00546d9dcf
anoninus <anoninuxt@gmail.com>
2026-05-28 07:42:52 +0530

    End now exists terminal mode

--- lua/user/config/ide/ide/toggleterm.lua
+++ lua/user/config/ide/ide/toggleterm.lua
@@ -3,7 +3,7 @@ vim.api.nvim_create_autocmd('TermOpen', {
     callback = function()
         local opts = { buffer = 0 }
 
-        vim.keymap.set('t', '<S-Tab>', [[<C-\><C-n>]], opts)
+        vim.keymap.set('t', '<End>', [[<C-\><C-n>G]], opts)
         vim.keymap.set('t', '<M-q>', '<cmd>close<CR>', opts)
 
         vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
@@ -20,7 +20,6 @@ local function toggle_terminal()
     if term_buf and vim.api.nvim_buf_is_valid(term_buf) then
         local cur = vim.api.nvim_get_current_buf()
         if cur == term_buf then
-            -- We're on the terminal, go back
             if prev_buf and vim.api.nvim_buf_is_valid(prev_buf) then
                 vim.cmd('buffer ' .. prev_buf)
             else
@@ -28,14 +27,12 @@ local function toggle_terminal()
             end
             return
         end
-        -- Switch to terminal
-        prev_buf = cur
+        if cur ~= term_buf then prev_buf = cur end
         vim.cmd('buffer ' .. term_buf)
         vim.cmd('startinsert')
         return
     end
 
-    -- First time
     prev_buf = vim.api.nvim_get_current_buf()
     vim.cmd('terminal')
     term_buf = vim.api.nvim_get_current_buf()
